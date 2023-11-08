33d982b0f0d3f39d589bfd33ab426106659eb4e6
bnm55vnn <90960555+vhoangminhnhat@users.noreply.github.com>
2023-11-08 13:51:05 +0700

    Sass project

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,21 +1,23 @@
-import { BrainCircuit } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import React from 'react'
-import { Poppins } from 'next/font/google';
+import { BrainCircuit } from "lucide-react";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Poppins } from "next/font/google";
 
-const poppins = Poppins({weight: '700', subsets: ["latin"]})
+const poppins = Poppins({ weight: "700", subsets: ["latin"] });
 
 interface LogoProps {
-    className?: string;
+  className?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({className}) => {
+const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
     <div className={cn("flex items-center", className)}>
-        <BrainCircuit color='#0ea5e9' size={40}/>
-        <span className={cn("ml-2 font-bold text-3xl", poppins.className)}>BrainWash</span>
+      <BrainCircuit color="#0ea5e9" size={40} />
+      <span className={cn("ml-2 font-bold text-3xl", poppins.className)}>
+        BrainWash
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
--- components/sidebars/index.tsx
+++ components/sidebars/index.tsx
@@ -4,6 +4,7 @@ import { useSidebarStore } from '@/stores/sidebar-store';
 import React from 'react'
 import Logo from '../logo';
 import SidebarsToggle from './sidebars-toggle';
+import { UserButton, useUser } from '@clerk/nextjs';
 
 interface SidebarsProps {
     className?: string;
@@ -13,6 +14,7 @@ interface SidebarsProps {
 
 const Sidebars: React.FC<SidebarsProps> = ({className, isProPlan, userLimitCount}) => {
     const {isMinimal} = useSidebarStore();
+    const {user} = useUser()
   return (
     <div className={cn(
         "text-white",
@@ -24,6 +26,39 @@ const Sidebars: React.FC<SidebarsProps> = ({className, isProPlan, userLimitCount
                     !isMinimal && <Logo/>
                 }
                 <SidebarsToggle/>
+            </div>
+        </div>
+        <div className='grow overflow-y-auto scroll-smooth scrollbar-none'>
+            <Navbar/>
+        </div>
+        <div className={cn(
+            "fixed bottom-8 left-4 right-4",
+            "lg:left-7 lg:right-auto",
+            isMinimal && "lg:left-3"
+        )}>
+            <div className='mb-4 p-4 rounded-lg bg-gray-900'>
+                <div className='mb-4 flex items-center'>
+                    <UserButton afterSignOutUrl='/'/>
+                    {
+                        !isMinimal &&
+                        <span className='text-sm ml-4'>
+                            {user?.emailAddresses?.[0]?.emailAddress}
+                        </span>
+                    }
+                </div>
+                {
+                    isMinimal && <div className='border-t border-t-gray-950 pt-2'>
+                        {
+                            !isProPlan && <div className='mb-4'>
+                                <div className='text-center mb-2 text-muted-foreground font-semibold'>
+                                    {
+                                        userLimitCount/{MAX_FREE_COUNTS}
+                                    }
+                                </div>
+                            </div>
+                        }
+                    </div>
+                }
             </div>
         </div>
     </div>
