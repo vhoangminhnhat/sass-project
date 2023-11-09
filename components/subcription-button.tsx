1f78892e9db9829898b04c14b3e2669ffe180e94
bnm55vnn <90960555+vhoangminhnhat@users.noreply.github.com>
2023-11-09 15:04:37 +0700

    Subcription button configurations

--- components/subcription-button.tsx
+++ components/subcription-button.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Button } from './ui/button';
+import { Sparkles } from 'lucide-react';
 
-const SubcriptionButton = () => {
+interface SubcriptionButtonProps {
+  className?: string;
+  isPro?: boolean
+}
+
+const SubcriptionButton: React.FC<SubcriptionButtonProps> = ({className, isPro}) => {
   return (
-    <div>SubcriptionButton</div>
+    <div className={className}>
+      <Button>
+        <span>{isPro ? "Manage Subcription" : "Upgrade to Pro"}</span>
+        <Sparkles/>
+      </Button>
+    </div>
   )
 }
 
